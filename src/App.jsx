8adcf92c31c2598f15ac803f6319358081e93033
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-09-24 16:51:18 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "/src/App.css";
 import Profile from "./components/Profile/Profile";
 import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import TransactionHistory from "./components/TransactionHistory/Transactionhistory";
 import userData from "./userData.json";
 import friends from "./friends.json";
 import transactions from "./transactions.json";
