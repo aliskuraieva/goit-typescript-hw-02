42d58864841db1f295b49965771b8d62c51d51e3
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-10-02 10:21:27 +0300

    set

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
 import "/src/App.css";
-import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import userData from "./userData.json";
-import friends from "./friends.json";
-import transactions from "./transactions.json";
 
 function App() {
   return (
     <>
-      <div>
-        <h2>Task 1 - Profile</h2>
-        <Profile
-          key={userData.tag}
-          name={userData.username}
-          tag={userData.tag}
-          location={userData.location}
-          avatar={userData.avatar}
-          stats={userData.stats}
-        />
-      </div>
-
-      <div>
-        <h2>Task 2 - Friend List</h2>
-        <FriendList friends={friends} />
-      </div>
-
-      <div>
-        <h2>Task 3 - Transaction History</h2>
-        <>
-          <TransactionHistory transactions={transactions} />
-        </>
-      </div>
+      <div></div>
     </>
   );
 }
