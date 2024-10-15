f66051e77c4f1dc43f2095646b12a7a749415d69
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-10-15 12:56:45 +0300

    set

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,8 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import "/src/App.css";
-import ContactForm from "./components/ContactForm/ContactForm";
-import SearchBox from "./components/SearchBox/SearchBox";
-import ContactList from "./components/ContactList/ContactList";
-import { nanoid } from "nanoid";
-import contactsData from "./contacts.json";
 
 const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const stringifiedContacts = localStorage.getItem("contacts");
-    const parsedContacts = JSON.parse(stringifiedContacts) ?? contactsData;
-    return parsedContacts;
-  });
-
-  const [filter, setFilter] = useState("");
-
-  useEffect(() => {
-    const stringifiedContacts = JSON.stringify(contacts);
-    localStorage.setItem("contacts", stringifiedContacts);
-  }, [contacts]);
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  const onAddContact = (formData) => {
-    const newContact = {
-      ...formData,
-      id: nanoid(),
-    };
-
-    setContacts((prevContacts) => [...prevContacts, newContact]);
-  };
-
-  const onDeleteContact = (contactId) => {
-    const updatedContacts = contacts.filter(
-      (contact) => contact.id !== contactId
-    );
-    setContacts(updatedContacts);
-  };
-
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm onAddContact={onAddContact} />
-      <SearchBox value={filter} setFilter={setFilter} />
-      <ContactList
-        contacts={filteredContacts}
-        onDeleteContact={onDeleteContact}
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -1,9 +1,9 @@
-import * as Yup from "yup";
+// import * as Yup from "yup";
 
-export const addContactSchema = Yup.object({
-  name: Yup.string()
-    .min(2, "Too Short!")
-    .max(20, "Too Long!")
-    .required("Required"),
-  number: Yup.string().required("Required"),
-});
+// export const addContactSchema = Yup.object({
+//   name: Yup.string()
+//     .min(2, "Too Short!")
+//     .max(20, "Too Long!")
+//     .required("Required"),
+//   number: Yup.string().required("Required"),
+// });
